b2402b1184923a9159950cba8f8d9fcc27d8e084
12ZainTahir <zaintahir0012@gmail.com>
2024-07-22 21:02:38 +0500

    Close button on tasks actions

--- resources/views/ticketview.blade.php
+++ resources/views/ticketview.blade.php
@@ -450,9 +450,12 @@ pathinfo($attachment->filename, PATHINFO_EXTENSION) == 'flv'
 
                  
 
-                  @if(Auth::user()->role==4)
+                  @if(Auth::user()->role==4||(Auth::user()->role<3&&$data22['ticketinfo'][0]->tasks_id))
                
                <button type="button" class="btn btn-warning float-right ml-1 replyandclose mr-1" value="Submit">Reply & Close Ticket</button>
+               @endif
+               @if(Auth::user()->role==4)
+
                <button type="button" class="btn btn-danger float-right sendtoocm" value="Submit">Send to OCM Support</button>
 
                   
