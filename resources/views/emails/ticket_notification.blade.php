9b1567da80a0f7f34f27f41df231df1811e85cac
12ZainTahir <zaintahir0012@gmail.com>
2024-08-13 11:17:10 +0500

    test

---
+++ resources/views/emails/ticket_notification.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ticket Reminder</title>
+</head>
+<body>
+    <p>Dear {{$username }},</p>
+    <p>This is a reminder to close your ticket with ID {{ $ticket_id }} and subject "{{ $subject }}".</p>
+    <p>Please take the necessary actions.</p>
+</body>
+</html>
